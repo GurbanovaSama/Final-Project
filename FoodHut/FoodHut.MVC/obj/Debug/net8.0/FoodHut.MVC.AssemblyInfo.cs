6766d4af6f01a47fff090cc128d495415f57da15
GurbanovaSama <samapg-ab205@code.edu.az>
2025-02-15 16:24:58 +0400

    added some changes

--- FoodHut/FoodHut.MVC/obj/Debug/net8.0/FoodHut.MVC.AssemblyInfo.cs
+++ FoodHut/FoodHut.MVC/obj/Debug/net8.0/FoodHut.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodHut.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7730f66ac33cdd3d0071b53574348c7ab565e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e558a2dc0864d18859a7f35f41092dcb6cb1ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodHut.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodHut.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
